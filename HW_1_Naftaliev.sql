8f2ae2996c600341f949cbb0525843908028796a
KonstantinNaftaliev <119065434+KonstantinNaftaliev@users.noreply.github.com>
2023-06-08 20:32:02 +0300

    Add files via upload

---
+++ HW_1_Naftaliev.sql
@@ -0,0 +1,7 @@
+SELECT * FROM hw_1_naftaliev.mobile_phone;
+
+SELECT ProductName, Manufacturer, Price FROM hw_1_naftaliev.mobile_phone
+WHERE ProductCount > 2;
+
+SELECT * FROM hw_1_naftaliev.mobile_phone
+WHERE Manufacturer = 'Samsung';
